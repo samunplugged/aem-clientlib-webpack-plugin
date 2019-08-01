dbf0b90391fdc5ff9516966f5410c0d615b04edf
Kris Erhardt <kris@kriserhardt.ca>
2019-08-01 13:21:20 -0700

    Add support for allowProxy and embed to createClientLibConfig

--- src/clientlib-template-engine.js
+++ src/clientlib-template-engine.js
@@ -3,8 +3,10 @@ import * as _ from 'lodash';
 const defaultTemplateStr = '<?xml version="1.0" encoding="UTF-8"?> \n\
 <jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" \n\
     jcr:primaryType="cq:ClientLibraryFolder" \n\
+    allowProxy="<%= allowProxy %>" \n\
     categories="[<%= categoryName %>]" \n\
-    dependencies="[<%= dependencies %>]"/>';
+    dependencies="[<%= dependencies %>]" \n\
+    embed="[<%= embed %>]"/>';
 
 export default class ClientlibTemplateEngine {
   constructor(_templateStr, _templateSettings) {
